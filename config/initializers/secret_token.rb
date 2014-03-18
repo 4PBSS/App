919a4a45aab7f544b646a5d7efbdcba051c5c915
Jerrod Long <jerrod@thinkinteractivedesign.com>
2014-03-17 23:15:04 -0400

    Added Testing Suite

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tax::Application.config.secret_key_base = '83df1aff47f412119250c1371486d0f664c6c3b1f89ed608cc3efc5f6ff7453b5549bf2de4358efdab86867d8a39559d090a5c08750baadba001bfd61b37adc8'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Tax::Application.config.secret_key_base = secure_token
