a134b5a99813f169525d08fed7a05e1f5cce7778
Jerrod Long <jerrod@thinkinteractivedesign.com>
2014-03-17 22:21:33 -0400

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tax::Application.config.secret_key_base = '83df1aff47f412119250c1371486d0f664c6c3b1f89ed608cc3efc5f6ff7453b5549bf2de4358efdab86867d8a39559d090a5c08750baadba001bfd61b37adc8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tax::Application.config.session_store :cookie_store, key: '_tax_session'
